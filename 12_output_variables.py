01a168c3791281f014b8929d47daf3a48905fcf1
Akash Barua <akashbaruaab969@gmail.com>
2025-07-14 19:16:32 +0600

    Add files via upload

---
+++ 12_output_variables.py
@@ -0,0 +1,10 @@
+# Demonstrating how to output variables using print() function
+
+x = "Sylhet Internation University (SIU)"
+print(x)  # Output: Sylhet Internation University (SIU)
+
+# Output multiple variables using commas
+x = "Shamimabad"
+y = "Bagbari"
+z = "Sylhet"
+print(x, y, z)  # Output: Shamimabad Bagbari Sylhet
