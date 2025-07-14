263d26005e3d0aa7a44bc1bb6febb6e2b1e2442c
Akash Barua <akashbaruaab969@gmail.com>
2025-07-14 17:43:53 +0600

    Add files via upload

---
+++ 04_variable_dynamic_typing.py
@@ -0,0 +1,6 @@
+# This code shows that variables in Python are dynamically typed
+
+x = 4         # x is initially an integer
+x = "Akash Barua"   # Now x is changed to a string
+
+print(x)      # Output the value of x, which is now "Akash Barua"
