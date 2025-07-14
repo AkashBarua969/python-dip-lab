ef0a67358006d2a6037d0080ef41a222af7d002a
Akash Barua <akashbaruaab969@gmail.com>
2025-07-14 17:26:04 +0600

    Update 04_variable_dynamic_typing.py

--- 04_variable_dynamic_typing.py
+++ 04_variable_dynamic_typing.py
@@ -3,4 +3,4 @@
 x = 4         # x is initially an integer
 x = "Akash Barua"   # Now x is changed to a string
 
-print(x)      # Output the value of x, which is now "Sally"
+print(x)      # Output the value of x, which is now "Akash Barua"
